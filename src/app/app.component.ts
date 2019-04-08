4f0f21f0fbb55f174be589ded58e441eaec3de13
tsteelematc <tsteele@madisoncollege.edu>
2019-04-08 18:05:02 -0500

    Demoed style and attribute binding...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+  myWidth = 250;
+
+  makeImageLarger() {
+    this.myWidth *= 1.3;
+  }
+
+  // Read-only/getter property..
+  get titleColor() {
+    return this.myWidth > 400 ? "red" : "blue";
+  }
 }
