bf6be91db9ead0b07450b78a872acb58deba0cef
tsteelematc <tsteele@madisoncollege.edu>
2019-04-08 19:07:08 -0500

    Hacked a working quiz service...
    Including DI into our root component... Etc...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private qSvc: QuizService) {
+    // Use the quiz service here, but... If it fails, the creation
+    // of the component fails : - (
+  }
+
+  ngOnInit() {
+    console.log(this.qSvc.getQuizzes());
+  };
+
   title = 'quiz-editor';
   myWidth = 250;
 
--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  getQuizzes() {
+    
+    // Dummy array of quiz objects...
+    return [
+      { name: 'Quiz 1', numberOfQuestion: 3 }
+      , { name: 'Quiz 2', numberOfQuestion: 0 }
+    ];
+  }
 }
