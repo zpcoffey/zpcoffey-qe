77157d5dd47e53ef213697e14ffc7739d7a07730
zpcoffey <43156754+zpcoffey@users.noreply.github.com>
2019-04-29 19:15:48 -0500

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
+import { trigger } from '@angular/animations';
+import { transition } from '@angular/animations';
+import { animate } from '@angular/animations';
+import { style } from '@angular/animations';
+import { keyframes } from '@angular/animations';
 
 interface QuizDisplay {
   name: string;
@@ -18,7 +23,29 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ left: '-30px', offset: 0.0 }),
+          style({ left: '-20px', offset: 0.25 }),
+          style({ left: '-10px', offset: 0.5 }),
+          style({ left: '-5px', offset: 0.75 }),
+          style({ left: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
@@ -32,6 +59,7 @@ export class AppComponent implements OnInit {
 
   selectQuiz(q: QuizDisplay) {
     this.selectedQuiz = q;
+    this.detailsAnimationState = "finalPosition";
   }
 
   addNewQuiz() {
@@ -54,7 +82,7 @@ export class AppComponent implements OnInit {
     ];
 
     // Select the newly added quiz.
-    this.selectedQuiz = newQuiz; 
+    this.selectQuiz(newQuiz);
   }
 
   removeQuestion(questionToRemove) {
@@ -69,6 +97,12 @@ export class AppComponent implements OnInit {
         name: "New Untitled Question"
       }
     ];
+  }
+
+  detailsAnimationState = "leftPosition";
+
+  detailsFromLeftAnimationComplete() {
+    this.detailsAnimationState = "leftPosition";
   }
 
   serviceDown = false;
